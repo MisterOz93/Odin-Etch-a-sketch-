5a2566a6cd8da3177ec4b2349c01ece06db72a02
Michael Mroz <mroz15@hotmail.com>
2020-10-10 22:45:30 -0400

    grid is set up and ready to go?

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 // create a grid of 16x16 square divs. Then nest them all in the existing div in html file. 
 // dust off css memory, use that to create columns and rows, then make function/loop for 16x16. 
 const container = document.querySelector('#container')
-const div = document.createElement('div');
-div.style.width = "50px";
-div.style.height = '50px';
-div.style.background = 'red';
-
-container.appendChild(div);
 
 
+function makeGrid (rows, cols) {
+for (i = 0; i < (rows * cols); i++) {
+    let div = document.createElement('div');
+    //div.innerText = i + 1; 
+    container.appendChild(div).className = "cell";
+}
+}
+makeGrid(16, 16);
